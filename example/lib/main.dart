0751d0f664a6a59f0ce5cbc1e840c8103c0ddde3
joshuauzor <joshuauzor10@gmail.com>
2023-02-03 07:06:27 +0100

    fix: clear example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:oneid_sdk_eg/login.dart';
+// ignore: depend_on_referenced_packages
+import 'package:oneid_flutter/oneid_flutter.dart';
+import 'package:oneid_sdk_eg/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,4 +20,47 @@ class MyApp extends StatelessWidget {
       home: const LoginWithOneID(),
     );
   }
+}
+
+class LoginWithOneID extends StatefulWidget {
+  const LoginWithOneID({Key? key}) : super(key: key);
+
+  @override
+  State<LoginWithOneID> createState() => _LoginWithOneIDState();
+}
+
+class _LoginWithOneIDState extends State<LoginWithOneID> {
+  OneIDPlugin? oneIdPlugin;
+
+  @override
+  void initState() {
+    oneIdPlugin = const OneIDPlugin(apiKey: 'apiKey');
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Center(
+        child: GestureDetector(
+          onTap: () async {
+            var res = await oneIdPlugin!.oneId(context: context);
+            await _navigateToHome(context: context, user: res);
+          },
+          child: const Text('login'),
+        ),
+      ),
+    );
+  }
+}
+
+Future<void> _navigateToHome({
+  required BuildContext context,
+  required User user,
+}) async {
+  await Navigator.push<void>(
+    context,
+    MaterialPageRoute(builder: (_) => Home(params: HomeParams(user: user))),
+  );
 }
